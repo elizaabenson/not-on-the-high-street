52f63a822d566a29c29c03daf13a1e0b2c0158b3
elizaabenson <83448688+elizaabenson@users.noreply.github.com>
2021-05-06 18:26:59 +0100

    Update code.py
    Read in the CSV file

--- code.py
+++ code.py
@@ -3,3 +3,11 @@ import csv
 from collections import defaultdict
 from collections import Counter
 
+#read in csv file
+columns = defaultdict(list) # each value in each column is appended to a list
+
+with open('C:/Users/frizz/OneDrive/Desktop/nypd-arrest-data-2018-1.csv') as f:
+    reader = csv.DictReader(f) # read rows into a dictionary format
+    for row in reader: # read a row as {column1: value1, column2: value2,...}
+        for (k,v) in row.items(): # go over each column name and value 
+            columns[k].append(v)
